cb2234c61091c58ee9c87459de5eeb798636b5c1
somozadev <somozadev@gmail.com>
2021-03-04 19:08:45 +0100

    Object setup

---
+++ Assets/Scripts/Enemies/IK_Enemie/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] Vector3 GotoPos;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        // Gizmos.color = Color.green;
+        // Gizmos.DrawWireSphere(GotoPos, 1f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, .2f);
+    }
+}
